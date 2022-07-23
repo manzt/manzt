ee0c0a84772376476e9e0373712cdc388739b0da
Trevor Manz <trevor.j.manz@gmail.com>
2022-07-23 14:19:50 -0400

    feat: add orcid, redirect to personal website

--- mod.ts
+++ mod.ts
@@ -1,16 +1,8 @@
 import { serve } from "https://deno.land/std@0.142.0/http/server.ts";
-import {
-	blue,
-	bold,
-	cyan,
-	gray,
-	green,
-	setColorEnabled,
-	yellow,
-} from "https://deno.land/std@0.147.0/fmt/colors.ts";
+import { bold, gray, green } from "https://deno.land/std@0.147.0/fmt/colors.ts";
 
 // deno-fmt-ignore
-let text = () => `\
+let text = `\
 ${green("   ╭───────────────────────────────────────────────────╮")}
 ${green("   │                                                   │")}
 ${green("   │")}               trevor manz  ${green("/")}  manzt               ${green("│")}
@@ -19,27 +11,13 @@ ${green("   │")}           phd student in bioinformatics           ${green("�
 ${green("   │")}         interested in data visualization          ${green("│")}
 ${green("   │")}             and open-source software              ${green("│")}
 ${green("   │                                                   │")}
-${green("   │")}    ${bold("twitter:")}  ${gray("https://twitter.com/")}${cyan("trevmanz")}         ${green("│")}
-${green("   │")}    ${bold(" github:")}  ${gray("https://github.com/")}${yellow("manzt")}             ${green("│")}
-${green("   │")}   ${bold("linkedin:")}  ${gray("https://linkedin.com/in/")}${blue("trevor-manz")}  ${green("│")}
-${green("   │")}   ${bold("     web:")}  ${cyan("https://trevorma.nz")}                  ${green("│")}
+${green("   │")}     ${bold("   orcid:")}  ${gray("orcid.org/")}${green("0000-0001-7694-5164")}      ${green("│")}
+${green("   │")}      ${bold("twitter:")}  ${gray("twitter.com/")}${green("trevmanz")}               ${green("│")}
+${green("   │")}      ${bold(" github:")}  ${gray("github.com/")}${green("manzt")}                   ${green("│")}
+${green("   │")}     ${bold("linkedin:")}  ${gray("linkedin.com/in/")}${green("trevor-manz")}        ${green("│")}
 ${green("   │                                                   │")}
 ${green("   ╰───────────────────────────────────────────────────╯")}
 `;
-
-let html = `\
-<!doctype html>
-<samp>
-	<p align="center">
-		<span>I'm a graduate student in bioinformatics interested in data visualization and open-source software.</span>
-		<br>
-		<br>
-		<a href="https://trevorma.nz">me</a> .
-		<a href="mailto:trevor_manz@g.harvard.edu">email</a> .
-		<a href="https://twitter.com/trevmanz">twitter</a> .
-		<a href="https://www.buymeacoffee.com/manzt">buy me a coffee</a>
-	</p>
-</samp>`;
 
 let tsconfig = {
 	"compilerOptions": {
@@ -64,6 +42,21 @@ let tsconfig = {
 	},
 };
 
+let _html = `\
+<!doctype html>
+<samp>
+	<p align="center">
+		<span>I'm a graduate student in bioinformatics interested in data visualization and open-source software.</span>
+		<br>
+		<br>
+		<a href="https://trevorma.nz">me</a> .
+		<a href="mailto:trevor_manz@g.harvard.edu">email</a> .
+		<a href="https://twitter.com/trevmanz">twitter</a> .
+		<a href="https://www.buymeacoffee.com/manzt">buy me a coffee</a>
+	</p>
+</samp>
+`;
+
 serve((req: Request) => {
 	let url = new URL(req.url);
 	if (url.pathname === "/tsconfig.json") {
@@ -72,12 +65,7 @@ serve((req: Request) => {
 		});
 	}
 	if (req.headers.get("Accept")?.includes("text/html")) {
-		return new Response(html, {
-			headers: { "content-type": "text/html" },
-		});
+		return Response.redirect("https://trevorma.nz");
 	}
-	if (url.searchParams.get("nocolor")) {
-		setColorEnabled(false);
-	}
-	return new Response(text());
+	return new Response(text);
 });
