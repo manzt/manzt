46b3a4e83e2c163c8f64d8e3cb232c2ff3933be2
Trevor Manz <trevor.j.manz@gmail.com>
2025-01-09 15:58:12 -0500

    Cleanup rethrow-unless docs

--- utils/rethrow-unless.js
+++ utils/rethrow-unless.js
@@ -12,20 +12,20 @@
  *
  * try {
  *   await db.query();
- * } catch (err) {
- *   rethrowUnless(err, DatabaseError, NetworkError);
- *   err // DatabaseError | NetworkError
+ * } catch (error) {
+ *   rethrowUnless(error, DatabaseError, NetworkError);
+ *   error; // DatabaseError | NetworkError
  * }
  * ```
  *
  * @template {ReadonlyArray<new (...args: any[]) => Error>} E
  * @param {unknown} error - The error to check
- * @param {E} errors - Expected error type(s)
+ * @param {E} ErrorClasses - Expected error type(s)
  * @throws The original error if it doesn't match expected type(s)
  * @returns {asserts error is E[number] extends new (...args: any[]) => infer R ? R : never}
  */
-export function rethrowUnless(error, ...errors) {
-	if (!errors.some((ErrorClass) => error instanceof ErrorClass)) {
+export function rethrowUnless(error, ...ErrorClasses) {
+	if (!ErrorClasses.some((ErrorClass) => error instanceof ErrorClass)) {
 		throw error;
 	}
 }
